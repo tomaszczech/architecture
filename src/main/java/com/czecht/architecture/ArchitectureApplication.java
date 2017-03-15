bf7dc83ee827ff397dc0f6b57f4bec5bcde45e91
Tomasz Czech <tomasz.czech@contractors.roche.com>
2017-03-15 17:10:32 +0100

    init

---
+++ src/main/java/com/czecht/architecture/ArchitectureApplication.java
@@ -0,0 +1,14 @@
+package com.czecht.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+public class ArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureApplication.class, args);
+	}
+}
